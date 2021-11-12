1860c90fa86e1c71f80758f432587f6e0cda108b
XieNaoban <22251525+XieNaoban@users.noreply.github.com>
2021-11-12 17:14:50 +0800

    breedingAge的获取从反射改为Mixin

---
+++ src/main/java/xienaoban/minecraft/bole/mixin/IMixinGoalSelector.java
@@ -0,0 +1,14 @@
+package xienaoban.minecraft.bole.mixin;
+
+import net.minecraft.entity.ai.goal.GoalSelector;
+import net.minecraft.entity.ai.goal.PrioritizedGoal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Set;
+
+@Mixin(GoalSelector.class)
+public interface IMixinGoalSelector {
+    @Accessor
+    Set<PrioritizedGoal> getGoals();
+}
